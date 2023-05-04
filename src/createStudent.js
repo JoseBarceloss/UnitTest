236b9c3677048c66a01a430605fc033ef70cdad0
Barcelos <barcelosps41710@gmail.com>
2023-05-04 02:09:54 -0300

    exercicio 5 completo

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  return {
+    name: name,
+    feedback: () => 'Eita pessoa boa!'
+  }
+};
 
 module.exports = createStudent;
+
